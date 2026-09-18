4a48b77b79f2bf7368776f5142182d6d8c24efbe
sa12384 <sangaywangmo1387@gmail.com>
2026-09-18 12:26:30 -0700

    Unit IV Lab 4: Order model, full CRUD, validation, aggregation, and transactions

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,7 @@
-const Restaurant = require('../models/Restaurant');
-exports.getHome = async (req, res) => {
- const restaurants = await Restaurant.getAllRestaurants();
- res.render('index', { title: 'Campus Eats', restaurants });
-};
+const Restaurant = require('../models/Restaurant'); 
+const Order = require('../models/Order');  
+exports.getHome = async (req, res) => {   
+    const restaurants = await Restaurant.getAllRestaurants();   
+    const stats = await Order.getStats();   
+    const popularItems = await Order.getPopularItems();   
+    res.render('index', { title: 'Campus Eats', restaurants, stats, popularItems }); }; 
--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,11 @@ module.exports = router;
 const menuController = require('../controllers/menuController');
 
 router.get('/restaurants/:id/menu', menuController.getMenuByRestaurant);
-const orderController = require('../controllers/orderController');
+const orderController = require('../controllers/orderController'); 
 
-router.post('/orders', orderController.createOrder);
+router.post('/orders', orderController.createOrder); 
+router.get('/orders/:id', orderController.getOrder); 
+
+router.post('/orders/:id/update', orderController.updateOrder); 
+
+router.post('/orders/:id/cancel', orderController.cancelOrder); 
